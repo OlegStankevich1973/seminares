97e50d9344bb09960744b23a64c878cf7b5acf95
Oleg Stankevich <stankeviz@mail.ru>
2022-07-11 17:33:43 +0300

    Программа возведения в квадрат числа

--- seminars/task001/Program.cs
+++ seminars/task001/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int outNumber = inputNumber*inputNumber;
+    Console.WriteLine(outNumber);
+}
