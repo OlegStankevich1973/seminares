638c3506c5c495b51cdb46ab6930ada19d73d94d
Oleg Stankevich <stankeviz@mail.ru>
2022-07-12 15:29:11 +0300

    Добавил программу вывода дня недели по числу(вариант 3)

---
+++ seminars/task006/Program.cs
@@ -0,0 +1,9 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(inputNumber));
+
+Console.WriteLine(outDayOfWeek);
+}
