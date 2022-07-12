02070cf4c0f0f9c1bd94b771251287b87e346f0e
Oleg Stankevich <stankeviz@mail.ru>
2022-07-12 13:40:40 +0300

    Добавил программу вывода дня недели по числу щт 1до 7

---
+++ seminars/task004/Program.cs
@@ -0,0 +1,20 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    string[]dayOffWeek = new string[7];
+    dayOffWeek[0] = "Понедельник";
+    dayOffWeek[1] = "Вторник";
+    dayOffWeek[2] = "Среда";
+    dayOffWeek[3] = "Четверг";
+    dayOffWeek[4] = "Пятница";
+    dayOffWeek[5] = "Суббота";
+    dayOffWeek[6] = "Воскресенье";
+
+    Console.WriteLine(dayOffWeek[inputNumber-1]);
+
+
+    
+
+}
