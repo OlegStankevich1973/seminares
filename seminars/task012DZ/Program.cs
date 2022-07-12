a012b0022a3f25dadc3890d30eea47063e9fa17b
Oleg Stankevich <stankeviz@mail.ru>
2022-07-13 01:00:00 +0300

    Программа нахождения максимального из двух

---
+++ seminars/task012DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+
+if(inputLineOne != null && inputLineTwo != null)
+{
+    int inputNumberOne = int.Parse(inputLineOne);
+    int inputNumberTwo = int.Parse(inputLineTwo);
+
+ Console.Write("max = ");
+ 
+    if(inputNumberOne > inputNumberTwo)
+
+         Console.Write(inputNumberOne);
+    else  
+         
+        Console.Write(inputNumberTwo); 
+
+}
