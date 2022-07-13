13c1fb33f4a1aef25542fbada4c503161cefbb4f
Oleg Stankevich <stankeviz@mail.ru>
2022-07-13 23:22:21 +0300

    Программа определения четного числа

---
+++ seminars/task14DZ1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число:  "); 
+string? inputLine = Console.ReadLine();
+string? outputLine = Console.ReadLine();
+
+
+if(inputLine != null);
+{   
+    int inputNumber = int.Parse(inputLine);
+    int outputNumber = int.Parse(inputLine);
+    int outputNumber = inputNumber%2;
+    
+     if(outputNumber =0)
+    {
+        Console.WriteLine("Да - чётное");
+    }   
+        
+    else
+    {
+        Console.WriteLine("Да - нечётное");
+    }         
+        
+              
+    
+}
