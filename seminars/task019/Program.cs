494be6d07e868f9935fd13d089a9a3dbe4ad9b31
Oleg Stankevich <stankeviz@mail.ru>
2022-07-16 17:37:19 +0300

    Программа вывода максимальной цифры числа из случайно выбранного вариант 3

---
+++ seminars/task019/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Random numberSintezator = new System.Random();
+char[] digits =numberSintezator.Next(10,100).ToString().ToCharArray();
+// выводим случайное число на экран
+Console.WriteLine(digits);
+int firstNumber = ((int)digits[0])-48;
+int secondNumber = ((int)digits[1])-48;
+// сравниваем по типу оператора(if и else)
+int resultNumber = firstNumber>secondNumber?resultNumber=firstNumber:resultNumber=secondNumber;
+Console.WriteLine(resultNumber);
