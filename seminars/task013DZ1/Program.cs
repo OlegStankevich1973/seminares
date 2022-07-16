4d57b046375665a0065efe747896e3f28bd446d3
Oleg Stankevich <stankeviz@mail.ru>
2022-07-16 13:40:42 +0300

    Программа поиска максимума из 3 чисел

--- seminars/task013DZ1/Program.cs
+++ seminars/task013DZ1/Program.cs
@@ -1,25 +1,26 @@
 ﻿string? inputLineOne = Console.ReadLine();
 string? inputLineTwo = Console.ReadLine();
 string? inputLineThree = Console.ReadLine();
-
-
-if(inputLineOne != null && inputLineTwo != null&& inputLineThree != null);
-
+if (inputLineOne != null && inputLineTwo != null && inputLineThree != null) ;
 {
-    int inputNumberOne = int.Parse(inputLineOne);
-    int inputNumberTwo = int.Parse(inputLineTwo);
-    int inputNumberThree = int.Parse(inputLineThree);
-    int maxNumber = inputNumberOne;
- 
-    if(inputNumberOne < inputNumberTwo)
-    {
-         maxNumber = inputNumberTwo;
-    } 
-         if(inputNumberTwo < inputNumberThree)
-
-         {   
-            maxNumber = inputNumberThree;
-         }
-          Console.WriteLine(maxNumber); 
-
+     int inputNumberOne = int.Parse(inputLineOne);
+     int inputNumberTwo = int.Parse(inputLineTwo);
+     int inputNumberThree = int.Parse(inputLineThree);
+     int maxNumber = inputNumberOne;
+     if (inputNumberOne < inputNumberTwo)
+     {
+          maxNumber = inputNumberTwo;
+          if (inputNumberTwo < inputNumberThree)
+          {
+               maxNumber = inputNumberThree;
+          }
+     }
+     else
+     {
+          if (inputNumberOne < inputNumberThree)
+          {
+          maxNumber = inputNumberThree;
+          }
+     }
+Console.WriteLine(maxNumber);
 }
---
+++ seminars/task016/Program.cs
@@ -0,0 +1,26 @@
+﻿string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+string? inputLineThree = Console.ReadLine();
+if (inputLineOne != null && inputLineTwo != null && inputLineThree != null) ;
+{
+int inputNumberOne = int.Parse(inputLineOne);
+int inputNumberTwo = int.Parse(inputLineTwo);
+int inputNumberThree = int.Parse(inputLineThree);
+int maxNumber = inputNumberOne;
+if (inputNumberOne < inputNumberTwo)
+{
+maxNumber = inputNumberTwo;
+if (inputNumberTwo < inputNumberThree)
+{
+maxNumber = inputNumberThree;
+}
+}
+else
+{
+if (inputNumberOne < inputNumberThree)
+{
+maxNumber = inputNumberThree;
+}
+}
+Console.WriteLine(maxNumber);
+}
