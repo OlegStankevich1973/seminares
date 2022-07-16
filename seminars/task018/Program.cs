aa66324aa1863ab79eeeba15cc86a64642388797
Oleg Stankevich <stankeviz@mail.ru>
2022-07-16 15:50:43 +0300

    Программа вывода случайного числа и определения его максимальной цифры вариант 2

---
+++ seminars/task018/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = new Random().Next(10, 100);
+
+string stringNum = number.ToString();
+Console.Write("The random number is: ");
+Console.WriteLine(number);
+Console.Write("The biggest digit is: ");
+if(stringNum[0] > stringNum[1])
+{
+    Console.WriteLine(stringNum[0]);
+}
+else
+{
+    Console.WriteLine(stringNum[1]);
+}
