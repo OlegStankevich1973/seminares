4dfc533f22819d9bddebc8484b1040814f4d8c5d
Oleg Stankevich <stankeviz@mail.ru>
2022-07-17 15:28:14 +0300

    Программа вывода третьей цифры числа и если ее нет говорит что ее нет

---
+++ seminars/task025DZ2/Program.cs
@@ -0,0 +1,21 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null);
+{
+    // создаем массив который разбивает число на цифры
+char[] M =inputLine.ToCharArray();
+    // сравниваю индексы цифр числа 
+    if(inputLine.Length >= 3)
+    {
+        Console.WriteLine(M[2]);
+    }
+    else
+    {
+    Console.WriteLine("Третьей цифры нет");
+    }
+    
+}
+        
+
+
+   
