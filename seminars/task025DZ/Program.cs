d358df846de127a34f831d6778ab535ea7581010
Oleg Stankevich <stankeviz@mail.ru>
2022-07-17 13:03:49 +0300

    Программа выдачи второй цифры трехзначного числа

---
+++ seminars/task025DZ/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// вариант 1
+void digitTensNumbers()
+{
+int number = new Random().Next(100,1000);
+Console.WriteLine("Число: "+number);
+int numberTensDigit = number/10;
+int numberSecondDigit = numberTensDigit%10;
+Console.WriteLine("Вторая цифра трехзначного числа: "+numberSecondDigit);
+}
+// вариант 2
+void digitTensNumbers1()
+{
+int number = new Random().Next(100,1000);
+    Console.Write("The random number is: ");
+    Console.WriteLine(number);
+    string stringNum = number.ToString();
+    Console.Write("The modified num is: ");
+    Console.Write(stringNum[1]);
+} 
+ digitTensNumbers ();
+ digitTensNumbers1 ();
