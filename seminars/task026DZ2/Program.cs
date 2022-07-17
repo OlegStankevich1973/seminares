7b2deb053e236b5fd89043bee2dd9688077bd2f3
Oleg Stankevich <stankeviz@mail.ru>
2022-07-17 15:49:18 +0300

    Программа вывода на экран второй цифры трехзначного числа

---
+++ seminars/task026DZ2/Program.cs
@@ -0,0 +1,24 @@
+﻿// вариант 1
+    void digitTensNumbers ()
+{
+int number = new Random().Next(100,1000);
+    Console.Write("The random number is: ");
+    Console.WriteLine(number);
+    string stringNum = number.ToString();
+    Console.Write("The modified num is: ");
+    Console.Write(stringNum[1]);
+}    
+// вариант 2
+    void digitTensNumbers1 ()
+{
+int number = new Random().Next(100,1000);
+    Console.Write("The random number is: ");
+    Console.WriteLine(number);
+    int num1 = number/10;
+    int num2 = num1%10;
+    Console.Write("The modified num is: ");
+    Console.WriteLine(num2);
+}
+    digitTensNumbers ();
+    digitTensNumbers1 ();
+
