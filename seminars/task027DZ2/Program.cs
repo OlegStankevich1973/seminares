12018381503edb063757fb1798054b1e4210a4ec
Oleg Stankevich <stankeviz@mail.ru>
2022-07-17 16:49:40 +0300

    Программа является ли день недели выходным

---
+++ seminars/task027DZ2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число,обозначающее день недели: ");
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+ int inputNumber = int.Parse(inputLine);
+
+    if(inputNumber == 6||inputNumber==7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет"); 
+    }
+}
