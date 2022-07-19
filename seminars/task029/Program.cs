11e9fcdfde963d22e4c2d12878fc5f8863762839
Oleg Stankevich <stankeviz@mail.ru>
2022-07-19 23:08:38 +0300

    Программа вывода параметров четверти плоскости в зависимости от ввода номера четверти

---
+++ seminars/task029/Program.cs
@@ -0,0 +1,18 @@
+﻿void printAnswer(int number)
+{
+    // смотря какую цифру четверти ввел пользователь выдаются параметры этой четверти
+    if(number==1) Console.WriteLine("Допустимо: x>0, y>0");
+    if(number==2) Console.WriteLine("Допустимо: x<0, y>0");
+    if(number==3) Console.WriteLine("Допустимо: x<0, y<0");
+    if(number==4) Console.WriteLine("Допустимо: x>0, y<0");
+}
+// создаем строковую переменную
+string? inputLine = Console.ReadLine();
+// проверяем ее на пустоту
+if(inputLine != null)
+{
+    // преобразуем строковую переменную в целочисленную
+    int inputNumber = int.Parse(inputLine);
+    // передаем ее в метод определения printAnswer
+    printAnswer(inputNumber);
+}
