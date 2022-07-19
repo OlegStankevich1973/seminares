53300996b572b8ad68157b75d7ba78546c488d27
Oleg Stankevich <stankeviz@mail.ru>
2022-07-20 01:45:21 +0300

    Добавил программу расчета длины отрезка по координатам его точек A и B

---
+++ seminars/task030/Program.cs
@@ -0,0 +1,29 @@
+﻿int coordXPointA;
+int coordYPointA;
+int coordXPointB;
+int coordYPointB;
+double lengthAB;
+// Метод считывания координат точек
+void readDataOfPoint()
+{
+    Console.WriteLine("Введите координату X точки A");
+    coordXPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки A");
+    coordYPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату X точки B");
+    coordXPointB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки B");
+    coordYPointB = int.Parse(Console.ReadLine());
+}
+// Метод вычисления расстояния между точками
+void calculationsLengthAB()
+{
+    lengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2));
+}
+readDataOfPoint();
+calculationsLengthAB();
+
+Console.WriteLine(lengthAB);
