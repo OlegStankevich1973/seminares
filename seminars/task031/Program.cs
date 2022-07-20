13626edfb0111cc4eeb9c824cee403890dac1707
Oleg Stankevich <stankeviz@mail.ru>
2022-07-21 01:27:20 +0300

    Программа ввода числа и вывода ряда этого до этого числа и второго ряда квадратов этих чисел

---
+++ seminars/task031/Program.cs
@@ -0,0 +1,24 @@
+﻿// создаем строковую переменную
+string? inputLineN =Console.ReadLine();
+// проверяем ее на null(пустоту)
+if (inputLineN != null)
+{
+    // меняем строковую на цельночисленную переменную
+    int numberN = int.Parse(inputLineN);
+    // определяем как будет печатать первая строка в данном случае сразу не касается второй строки
+    string lineN = "";
+    
+    // string lineNN = string.Empty;
+    string lineNN = "";
+    // определяем первое число выполнения программы
+    int s = 1;
+    while (s <= numberN)
+    {
+        lineN = lineN + s + "";
+        lineNN = lineNN + (s * s).ToString() + "";
+        s++;
+    }
+        Console.WriteLine(lineN);
+        Console.WriteLine(lineNN);
+    
+}
