16433b0dd7d190f405e6a8dcf614056be8282783
Oleg Stankevich <stankeviz@mail.ru>
2022-07-21 19:37:57 +0300

    Программа расчета длины отрезка по трем координатам

---
+++ seminars/task032DZ3/Program.cs
@@ -0,0 +1,40 @@
+﻿int coordXPointA;
+int coordYPointA;
+int coordZPointA;
+int coordXPointB;
+int coordYPointB;
+int coordZPointB;
+double lengthAB;
+// Метод считывания координат точек
+void readDataOfPoint()
+{
+    Console.WriteLine("Введите координату X точки A");
+    coordXPointA = int.Parse(Console.ReadLine());
+    
+    Console.WriteLine("Введите координату Y точки A");
+    coordYPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Z точки A");
+    coordZPointA = int.Parse(Console.ReadLine());
+
+
+    Console.WriteLine("Введите координату X точки B");
+    coordXPointB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки B");
+    coordYPointB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Z точки B");
+    coordZPointB = int.Parse(Console.ReadLine());
+
+   
+}
+// Метод вычисления расстояния между точками
+void calculationsLengthAB()
+{
+    lengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2)+ Math.Pow((coordZPointA - coordZPointB), 2));
+}
+readDataOfPoint();
+calculationsLengthAB();
+
+Console.WriteLine(lengthAB);
