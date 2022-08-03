b3385e4c887c801e6e59d8bde2d850a452ce5391
Oleg Stankevich <stankeviz@mail.ru>
2022-08-03 21:32:58 +0300

    Program.cs

---
+++ seminars/task036/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число :");
+
+int inputNumber = int.Parse(Console.RedLine());
+void VariantSimple()
+{
+    int sumOfNumbers = 0;
+    for (int i = 1; i <= inputNumber; i++)
+    {
+        sumOfNumbers += i;
+        // sumOfNumbers=sumOfNumbers+i;
+    }
+    Console.WriteLine("Сумма чисел от 1 до" + inputNumber + "=" + sumOfNumbers);
+
+}
+
+VariantSimple();
