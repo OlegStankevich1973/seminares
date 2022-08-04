63bc1e7d7d2eab0b8e8b498578ca9165290b22f1
Oleg Stankevich <stankeviz@mail.ru>
2022-08-04 21:30:45 +0300

    Программа вычисления факториала числа со временем

---
+++ seminars/task038/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число :");
+string inputLine = Console.ReadLine();
+long inputNumber = long.Parse(inputLine);
+Console.WriteLine(sumNums(inputNumber));
+DateTime timePoint = DateTime.Now;
+timePoint = DateTime.Now;
+long sumNums(long num)
+{
+    long sum = 1;
+    for (long i = 1; i <= inputNumber; i++)
+    {
+        //   sum*=i;
+        sum = sum * i;
+    }
+    return sum;
+}
+Console.WriteLine(DateTime.Now - timePoint);
