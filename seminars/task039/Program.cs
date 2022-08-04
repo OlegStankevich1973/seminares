5712ce3b0d1c8acac90b833a3c50664fa66bccfa
Oleg Stankevich <stankeviz@mail.ru>
2022-08-05 00:58:54 +0300

    Программа формулы рекурсии с подсчетом времени расчета

---
+++ seminars/task039/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите число :");
+string inputLine = Console.ReadLine();
+long inputNumber = long.Parse(inputLine);
+Console.WriteLine(mulRec(inputNumber));
+DateTime timePoint = DateTime.Now;
+timePoint = DateTime.Now;
+long mulRec(long num)
+{
+    // тк 0!=1
+    if (num == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        // формула рекурсии n!=(n-1)*n
+        return num*mulRec(num-1);
+    }
+}
+
+
+Console.WriteLine(DateTime.Now - timePoint);
