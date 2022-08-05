3e5d61f77497e38d16914e5debabc8f461804484
Oleg Stankevich <stankeviz@mail.ru>
2022-08-06 00:08:52 +0300

    Программа вывода массива из 8 чисел произвольной последовательности из 0 и 1

---
+++ seminars/task040/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Random numberSintezator = new Random();
+// создание массива из 8 чифр 
+void VariantNaive()
+{
+    int i = 0;
+    Console.Write("[");
+    // убираем последний элемент чтобы потом убрать последнюю запятую при печати
+    while (i < 8 - 1)
+    {
+        // цифры массива 0 и 1(2 не входит) и запятые между ними
+        Console.Write(numberSintezator.Next(0,2)+",");
+        i++;
+    }
+    // распечатываем последний элемент отдельно
+    Console.Write(numberSintezator.Next(0,2));
+
+    Console.Write("]");
+
+}
+VariantNaive();
