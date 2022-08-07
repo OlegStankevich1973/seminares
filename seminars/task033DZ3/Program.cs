eb5eabaf03d298adbe7f4ad6fbbfc1aa22968897
Oleg Stankevich <stankeviz@mail.ru>
2022-08-07 23:20:35 +0300

    Программа построения ряда чисел и ряда этих чисел  в 3 степени

--- seminars/task033DZ3/Program.cs
+++ seminars/task033DZ3/Program.cs
@@ -1,6 +1,7 @@
-﻿// Console.Clear();
+﻿// Програама вывода ряда чисел и их кубов
+Console.Clear();
 // создаем строковую переменную
-string? inputLineN =Console.ReadLine();
+string? inputLineN = Console.ReadLine();
 // проверяем ее на null(пустоту)
 if (inputLineN != null)
 {
@@ -9,17 +10,17 @@ if (inputLineN != null)
     // определяем как будет печатать первая строка в данном случае сразу не касается второй строки
     string lineN = " ";
 
-     string lineNNN = " ";
+    string lineNNN = " ";
     // определяем первое число выполнения программы
     int s = 1;
-    
-    while (s < numberN+1)
+
+    while (s < numberN + 1)
     {
         lineN = lineN + s + " ";
         lineNNN = lineNNN + (s * s * s).ToString() + " ";
         s++;
     }
-        Console.WriteLine(lineN);
-        Console.WriteLine(lineNNN);
+    Console.WriteLine(lineN);
+    Console.WriteLine(lineNNN);
 
-} 
+}
