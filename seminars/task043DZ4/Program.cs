5f420afb5a990d26ccab1eec07641f27e91fdc63
Oleg Stankevich <stankeviz@mail.ru>
2022-08-08 01:21:50 +0300

    Программа создания массива любой длини и с любым диапазоном случайных чисел два варианта ручного ввода

--- seminars/task043DZ4/Program.cs
+++ seminars/task043DZ4/Program.cs
@@ -1,46 +1,62 @@
 ﻿
-
-// System.Random numberSintezator = new Random();
-// // создание массива из 8 чифр 
-// void VariantNaive()
-// {
-//     int i = 0;
-//     Console.Write("[");
-//     // убираем последний элемент чтобы потом убрать последнюю запятую при печати
-//     while (i < 8 - 1)
-//     {
-//         // цифры массива 0 и 1(2 не входит) и запятые между ними
-//         Console.Write(numberSintezator.Next(0,2)+",");
-//         i++;
-//     }
-//     // распечатываем последний элемент отдельно
-//     Console.Write(numberSintezator.Next(0,2));
-
-//     Console.Write("]");
-
-// }
+// Программа создания любого массива с любым диапазоном случайных чисел
 Console.Clear();
-// создаем строковую переменную
-string? inputLineN =Console.ReadLine();
-// проверяем ее на null(пустоту)
-if (inputLineN != null)
+void VariantOne()
 {
-    // меняем строковую на цельночисленную переменную
-    int numberN = int.Parse(inputLineN);
-    // определяем как будет печатать первая строка в данном случае сразу не касается второй строки
-    string lineN = " ";
-
-     string lineNNN = " ";
-    // определяем первое число выполнения программы
-    int s = 1;
-
-    while (s < numberN+1)
+    Console.WriteLine("Введите длину массива");
+    int lengthArray = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите начальный диапазон чисел массива");
+    int initNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите конечный диапазон чисел массива");
+    int finalNumber = int.Parse(Console.ReadLine());
+        System.Random numberSintezator = new Random();
+   
+    int i = 0;
+    Console.Write("[");
+    // убираем последний элемент чтобы потом убрать последнюю запятую при печати(задаём диапазон массива)
+    while (i < lengthArray - 1)
     {
-        lineN = lineN + s + " ";
-        lineNNN = lineNNN + (s * s * s).ToString() + " ";
-        s++;
+        // цифры массива от0 до 99(100 не входит) и запятые между ними
+        Console.Write(numberSintezator.Next(initNumber, finalNumber) + ",");
+        i++;
     }
-        Console.WriteLine(lineN);
-        Console.WriteLine(lineNNN);
+    // распечатываем последний элемент отдельно
+    Console.Write(numberSintezator.Next(initNumber, finalNumber));
 
-} 
+    Console.WriteLine("]");
+    Console.WriteLine("Диапазон значений элементов " + "(" + initNumber + "," + (finalNumber) + ")");
+    Console.WriteLine("Длина элементов массива равна " + lengthArray + " элементов");
+
+}
+VariantOne();
+void VariantTwo()
+{ // Программа создания любого массива с любым диапазоном чисел выдаваемых случайно
+// (ввод данных вручную в программе)
+    System.Random numberSintezator = new Random();
+    // создание синтезатором случайных цифр
+
+    int initNumber = 0;
+    // вводим конечный диапазон чисел
+    int finalNumber = 100;
+    // вводим длину массива
+    int lengthArray = 8;
+    int i = 0;
+    Console.Write("[");
+    // убираем последний элемент чтобы потом убрать последнюю запятую при печати(задаём диапазон массива)
+    while (i < lengthArray - 1)
+    {
+        // цифры массива от0 до 99(100 не входит) и запятые между ними
+        Console.Write(numberSintezator.Next(initNumber, finalNumber) + ",");
+        i++;
+    }
+    // распечатываем последний элемент отдельно
+    Console.Write(numberSintezator.Next(initNumber, finalNumber));
+
+    Console.WriteLine("]");
+    Console.WriteLine("Диапазон значений элементов " + "(" + initNumber + "," + (finalNumber) + ")");
+    Console.WriteLine("Длина элементов массива равна " + lengthArray + " элементов");
+
+}
+VariantTwo();
+
+
