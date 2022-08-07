426e34646043794842273a47a7f3914bd6d2c745
Oleg Stankevich <stankeviz@mail.ru>
2022-08-07 23:03:18 +0300

    Программа построения ряда чисел и ряда в 3 степени этих чисел

--- seminars/task043DZ4/Program.cs
+++ seminars/task043DZ4/Program.cs
@@ -1,4 +1,25 @@
-﻿// Console.Clear();
+﻿
+
+// System.Random numberSintezator = new Random();
+// // создание массива из 8 чифр 
+// void VariantNaive()
+// {
+//     int i = 0;
+//     Console.Write("[");
+//     // убираем последний элемент чтобы потом убрать последнюю запятую при печати
+//     while (i < 8 - 1)
+//     {
+//         // цифры массива 0 и 1(2 не входит) и запятые между ними
+//         Console.Write(numberSintezator.Next(0,2)+",");
+//         i++;
+//     }
+//     // распечатываем последний элемент отдельно
+//     Console.Write(numberSintezator.Next(0,2));
+
+//     Console.Write("]");
+
+// }
+Console.Clear();
 // создаем строковую переменную
 string? inputLineN =Console.ReadLine();
 // проверяем ее на null(пустоту)
@@ -12,7 +33,7 @@ if (inputLineN != null)
      string lineNNN = " ";
     // определяем первое число выполнения программы
     int s = 1;
-    
+
     while (s < numberN+1)
     {
         lineN = lineN + s + " ";
