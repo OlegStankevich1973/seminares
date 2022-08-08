c85d677ad500a65b92afd124c74baa6fde4a2286
Oleg Stankevich <stankeviz@mail.ru>
2022-08-08 19:21:53 +0300

    Программа построения ряда чисел и вывода их квадратов

--- seminars/task034DZ3/Program.cs
+++ seminars/task034DZ3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Програама вывода ряда чисел и их квадратов
+Console.Clear();
+// создаем строковую переменную
+string? inputLineN = Console.ReadLine();
+// проверяем ее на null(пустоту)
+if (inputLineN != null)
+{
+    // меняем строковую на цельночисленную переменную
+    int numberN = int.Parse(inputLineN);
+    // определяем как будет печатать первая строка в данном случае сразу не касается второй строки
+    string lineN = " ";
+
+    string lineNN = " ";
+    // определяем первое число выполнения программы
+    int s = 1;
+
+    while (s < numberN + 1)
+    {
+        lineN = lineN + s + " ";
+        lineNN = lineNN + (s * s ).ToString() + " ";
+        s++;
+    }
+    Console.WriteLine(lineN);
+    Console.WriteLine(lineNN);
+}
