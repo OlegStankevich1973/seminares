41896e19ceebe5dd8886539a5f15f1c6ca6eb82d
Oleg Stankevich <stankeviz@mail.ru>
2022-08-14 22:19:12 +0300

    Программа подсчета положительных элементов

---
+++ seminars/task057DZ6/Program.cs
@@ -0,0 +1,59 @@
+﻿// //=================================================================================
+// // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+// //==================================================================================
+//Метод возвращает массив заполненый случайными числами 
+int[] FillingArray(int arrayLength)
+{
+    int[] outArray = new int[arrayLength];
+    int i = 0;
+    System.Random numberSintezator = new System.Random();
+    while (i < arrayLength)
+    {
+        outArray[i] = numberSintezator.Next(-100, 100);
+        i++;
+    }
+    return outArray;
+}
+
+//Метод печатает массив
+void PrintIntArray(int[] inputArray)
+{
+    //Буфферная переменная
+    int i = 0;
+    //Пробегаем все элементы массива
+    while (i < inputArray.Length - 1)
+    {
+        //Выводим элемент массива
+        Console.Write(inputArray[i] + ", ");
+        //Увеличиваем инкремент
+        i++;
+    }
+    //Выводим элемент массива
+    Console.WriteLine(inputArray[i]);
+}
+
+//Метод считает положительные элементы массива 
+int ColculateTask(int[] inputArray)
+{
+    int resultCount = 0;
+    int i = 0;
+    while (i < inputArray.Length)
+    {
+        if (inputArray[i] >= 0)
+        {
+            resultCount++;// суммируем элементы соответствующие условию
+        }
+        i++;
+    }
+    return resultCount;
+}
+
+
+int[] buferArray = FillingArray(17);
+PrintIntArray(buferArray);
+Console.WriteLine("Количество подходящих элементов " + (ColculateTask(buferArray)));
+// int[] positivArray = ColculateTask();
+// Console.WriteLine(ColculateTask(positivArray));
