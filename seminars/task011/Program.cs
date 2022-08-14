9c09c115191365209742d23e23320187a44ed0d2
Oleg Stankevich <stankeviz@mail.ru>
2022-08-14 23:04:18 +0300

    Программа ввода числа и вывода проследней цифры используя массив

--- seminars/task011/Program.cs
+++ seminars/task011/Program.cs
@@ -1,8 +1,8 @@
 ﻿string? inputLine = Console.ReadLine();
 
-if(inputLine != null)
+if (inputLine != null)
 {
-char[] M =inputLine.ToCharArray();
+    char[] M = inputLine.ToCharArray();
 
-Console.WriteLine(M[2]);
+    Console.WriteLine(M[2]);
 }
