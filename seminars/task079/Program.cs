69c87c25dff62c2539390c1a2e8e41f529ac866a
Oleg Stankevich <stankeviz@mail.ru>
2022-08-24 19:59:46 +0300

    Программа вводим в консоль значение N,программа выдает все натуральные числа от 1 до N

--- seminars/task079/Program.cs
+++ seminars/task079/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿// Задайте значение N.И напишите программу которая выводит все натуральные числа от 1 до N
+Console.Clear();
+
+int ReadData()
+{
+    Console.WriteLine("Задайте число: ");
+    return int.Parse(Console.ReadLine());
+}
+
+//Стек
+//1 4 - 3 out
+//2 3 - 2 out 2
+//3 2 -   out 1
+
+// метод рекурсии
+int NuturalNumberPrinter(int num)
+{
+    if(num == 2 )return 1;
+    else
+    {
+        --num;
+        Console.WriteLine(num);
+        Console.Write(NuturalNumberPrinter(num) + ", ");
+    }
+    return num;
+}
+
+
+int inputNumber = ReadData();
+NuturalNumberPrinter(inputNumber+2);
