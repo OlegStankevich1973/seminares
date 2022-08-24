676188fb0fa8c610fc26ae975510e24273d719fb
Oleg Stankevich <stankeviz@mail.ru>
2022-08-25 00:48:08 +0300

    Программа возведения в степень n числа m методом рекурсии

---
+++ seminars/task082DZ9/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Clear();
+int numberM = ReadData("Задайте число M");
+int numberN = ReadData("Задайте число N");
+// условия включения метода рекурсии
+if (numberM < numberN)
+{
+    NaturalNamberPrinter(numberM, numberN);
+}
+else
+{
+    NaturalNamberPrinter(numberN, numberM);
+}
+
+// метод ввода с консоли
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+
+// метод рекурсии
+void NaturalNamberPrinter(int m, int n)
+{
+    if (m - 1 == n) return;//-1 чтобы вывело все цифры на печать
+    Console.Write(m + " ");//m+ вывод всх чисел которые создаются рекурсией (n не изменяется)
+    NaturalNamberPrinter(m + 1, n);//+1 чтобы числа от меньшего m двигались к большему n
+}
