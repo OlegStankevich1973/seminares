8802ff3fcbe0b25ae50b200b9a0276e75dbb7d83
Oleg Stankevich <stankeviz@mail.ru>
2022-08-24 20:58:01 +0300

    Программа вывода всех натуральных чисел в заданном диапазоне

---
+++ seminars/task077/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Clear();
+int numberM = ReadData("Задайте число M");
+int numberN = ReadData("Задайте число N");
+// условия включения метода рекурсии
+if (numberM < numberN)
+{
+    NaturalNamberPrinter(numberM, numberN);
+}
+else
+{
+    NaturalNamberPrinter(numberN, numberM);
+}
+
+// метод ввода с консоли
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+// 3 6
+// 4 6
+// 5 6
+// 6 6
+// 7 6
+// int sum =0;
+// метод рекурсии
+void NaturalNamberPrinter(int m, int n)
+{
+    if (m - 1 == n) return;//-1 чтобы вывело все цифры на печать
+    Console.Write(m + " ");
+    NaturalNamberPrinter(m + 1, n);//=1 чтобы числа от меньшего двигались к большему
+}
